a9de66776dd2927940769952cc7653b9a5e17e68
Hcaglar32 <83988785+Hcaglar32@users.noreply.github.com>
2024-11-21 14:00:56 +0300

    update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchAllProducts = async () => {
   try {
     const response = await axios.get('https://hcaglar32.github.io/case-laravel/public/data.json');
-    return response.data.data.products;
+    return response.data.products;
   } catch (error) {
     console.error("Error fetching products:", error);
     throw error;
